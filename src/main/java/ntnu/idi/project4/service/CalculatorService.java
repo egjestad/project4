358d49146b3a9659d3d77e54b573087a113f3c6a
egjestad <egjestad@gmail.com>
2025-02-24 19:35:18 +0100

    Calculation service

--- src/main/java/ntnu/idi/project4/service/CalculatorService.java
+++ src/main/java/ntnu/idi/project4/service/CalculatorService.java
@@ -2,8 +2,46 @@ package ntnu.idi.project4.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CalculatorService {
+
+  public double calculate(String expression) {
+    List<String> tokens = tokenize(expression);
+    double result = Double.parseDouble(tokens.getFirst());
+    for (int i = 1; i < tokens.size(); i += 2) {
+      String operator = tokens.get(i);
+      double operand = Double.parseDouble(tokens.get(i + 1));
+      result = calc(result, operand, operator);
+    }
+    return result;
+  }
+
+  private List<String> tokenize(String expression) {
+    List<String> tokens = new ArrayList<>();
+    int i = 0;
+    while (i< expression.length()) {
+      char c = expression.charAt(i);
+      if (Character.isDigit(c)|| c == '.') {
+        StringBuilder sb = new StringBuilder();
+        while ((i < expression.length()) && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+          sb.append(expression.charAt(i));
+          i++;
+        }
+        tokens.add(sb.toString());
+      }else if (c == '+' || c == '-' || c == 'x' || c == '/') {
+        tokens.add(String.valueOf(c));
+        i++;
+      } else if (Character.isWhitespace(c)) {
+        i++;
+      } else {
+        throw new IllegalArgumentException("Invalid character: " + c);
+      }
+    }
+    return tokens;
+  }
 
   /**
    * Calculate a result from two numbers with an operator
@@ -23,10 +61,5 @@ public class CalculatorService {
       throw new IllegalArgumentException("Operator not supported");
     }
   }
-
-
-
-
-
 
 }
