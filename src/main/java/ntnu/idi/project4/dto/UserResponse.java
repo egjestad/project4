c5005cc122875d6d2e5a8657618ad91a3c772dfa
egjestad <egjestad@gmail.com>
2025-03-13 14:20:08 +0100

    UserDTOs

--- src/main/java/ntnu/idi/project4/dto/UserResponse.java
+++ src/main/java/ntnu/idi/project4/dto/UserResponse.java
@@ -1,4 +1,38 @@
 package ntnu.idi.project4.dto;
 
 public class UserResponse {
+  private String username;
+  private int userId;
+
+  public UserResponse() {
+  }
+
+  public UserResponse(String username) {
+    this.username = username;
+  }
+
+  public UserResponse(String username, int userId) {
+    this.username = username;
+    this.userId = userId;
+  }
+
+  public UserResponse(int userId) {
+    this.userId = userId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
 }
