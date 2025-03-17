2f8b0ccddd65f63b4be86647cc40bf0e644497a5
egjestad <egjestad@gmail.com>
2025-03-17 13:22:45 +0100

    Added usertoken and autentication to UserService class

--- src/main/java/ntnu/idi/project4/service/UserService.java
+++ src/main/java/ntnu/idi/project4/service/UserService.java
@@ -1,7 +1,10 @@
 package ntnu.idi.project4.service;
 
+import ntnu.idi.project4.exeptions.InncorectPasswordExeption;
+import ntnu.idi.project4.exeptions.UserNotFoundExeption;
 import ntnu.idi.project4.model.User;
 import ntnu.idi.project4.repo.UserRepository;
+import ntnu.idi.project4.security.TokenUtil;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
@@ -12,11 +15,32 @@ import java.util.Optional;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final TokenUtil tokenUtil;
 
   private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-  public UserService(UserRepository userRepository) {
+  public UserService(UserRepository userRepository, TokenUtil tokenUtil) {
     this.userRepository = userRepository;
+    this.tokenUtil = tokenUtil;
+  }
+
+  public String authenticateUser(String username, String password) {
+    logger.info("Authenticating user: " + username);
+    User user = userRepository.findByUsername(username);
+    logger.info("Found user: " + user);
+    if (user != null) {
+      logger.info("User password: " + user.getPassword());
+      if (user.getPassword().equals(password)) {
+        logger.info("User authenticated: " + user);
+        return tokenUtil.generateToken(user.getId());
+      } else {
+        logger.info("Password incorrect");
+        throw new InncorectPasswordExeption("Password incorrect");
+      }
+    } else {
+      logger.info("User not found");
+      throw new UserNotFoundExeption("User not found");
+    }
   }
 
   public void registerUser(String username, String password) {
