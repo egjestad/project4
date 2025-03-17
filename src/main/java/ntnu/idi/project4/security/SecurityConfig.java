f7221037039728318082f82c8cfbaa91be7be574
egjestad <egjestad@gmail.com>
2025-03-17 13:21:45 +0100

    New SecurityConfig class

---
+++ src/main/java/ntnu/idi/project4/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package ntnu.idi.project4.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+/*
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/token", "/register", "/login").permitAll()
+                    .anyRequest().authenticated())
+            .addFilterBefore(new JWTAuthFilter(tokenUtil), UsernamePasswordAuthenticationFilter.class);
+
+    http.cors(cors -> cors.configurationSource(request -> {
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+      config.setAllowedHeaders(List.of("*"));
+      return config;
+    }));
+
+ */
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  http.csrf(csrf -> csrf.disable())
+          .cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(List.of("http://localhost:5173"));
+            config.setAllowedMethods(List.of("GET", "POST"));
+            config.setAllowedHeaders(List.of("*"));
+            return config;
+          }))
+          .authorizeHttpRequests(auth -> auth
+                  .requestMatchers("/register", "/login").permitAll()
+                  .anyRequest().authenticated()
+          );
+  return http.build();
+}
+
+
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+}
